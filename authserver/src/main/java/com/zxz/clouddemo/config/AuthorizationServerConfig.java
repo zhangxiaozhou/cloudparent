5702338c32a5ceb510bd4f7d63b254e6a31403ef
zhangxaiozhou <7102405072@qq.com>
2019-04-02 02:58:25 +0800

    init

--- authserver/src/main/java/com/zxz/clouddemo/config/AuthorizationServerConfig.java
+++ authserver/src/main/java/com/zxz/clouddemo/config/AuthorizationServerConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
