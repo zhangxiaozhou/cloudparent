617574fbc0138a4aa12ac5567381ba82bfb6c512
zhangxaiozhou <7102405072@qq.com>
2019-04-02 11:20:06 +0800

    configserver

--- configserver/src/main/java/com/zxz/clouddemo/ConfigServer.java
+++ configserver/src/main/java/com/zxz/clouddemo/ConfigServer.java
@@ -2,12 +2,14 @@ package com.zxz.clouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class AuthServer
+public class ConfigServer
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(AuthServer.class);
+        SpringApplication.run(ConfigServer.class);
     }
 }
