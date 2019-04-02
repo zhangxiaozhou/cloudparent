28d03dded0ba747bda65dd0c23b5fc3b378b2ebd
zhangxaiozhou <7102405072@qq.com>
2019-04-02 17:36:28 +0800

    feign添加hystrix支持熔断机制

--- service1/src/main/java/com/zxz/clouddemo/Service1.java
+++ service1/src/main/java/com/zxz/clouddemo/Service1.java
@@ -3,12 +3,14 @@ package com.zxz.clouddemo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 @SpringBootApplication
 public class Service1
 {
--- service1/src/main/java/com/zxz/clouddemo/feignclients/Service2Client.java
+++ service1/src/main/java/com/zxz/clouddemo/feignclients/Service2Client.java
@@ -1,11 +1,21 @@
 package com.zxz.clouddemo.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "service2")
+@FeignClient(value = "service2", fallback = Service2ClientFallback.class)
 public interface Service2Client {
 
     @RequestMapping("/consul/getHost")
     String getHost();
+}
+
+@Component
+class Service2ClientFallback implements Service2Client{
+
+    @Override
+    public String getHost() {
+        return "unknow host!";
+    }
 }
